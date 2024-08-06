91b5026d2ebe646e4f4eebc30fdc78d73f51457d
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2024-08-06 22:23:04 +0700

    feat: Navbar and logo

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent {
+  navbarfixed: boolean = false;
+
+  @HostListener('window:scroll', ['$event']) onscroll() {
+    if (window.scrollY > 100) {
+      this.navbarfixed = true;
+    } else {
+      this.navbarfixed = false;
+    }
+  }
+}
