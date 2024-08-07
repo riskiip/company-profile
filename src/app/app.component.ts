2e432fe43e3020e6698583dea1afd2357e9cd860
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2024-08-08 00:08:21 +0700

    feat: About section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HomeComponent, ProductComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    HomeComponent,
+    ProductComponent,
+    AboutComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
