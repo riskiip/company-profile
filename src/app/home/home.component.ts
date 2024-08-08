d3d3e03abf319d92882652e4d46f30c438bd6331
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2024-08-09 01:18:29 +0700

    feat: wip pinpoint map

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  @Output() aboutEmitter = new EventEmitter();
 
+  onAboutClick(anchor: string) {
+    this.aboutEmitter.emit(anchor);
+  }
 }
